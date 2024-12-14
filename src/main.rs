23cadabf48d0b2628ea709f3d728c2c558f2a188
edj2010 <evan.durston.johnson@gmail.com>
2024-12-14 13:53:27 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![feature(test)]
+
+mod day_template;
+
+#[allow(dead_code)]
+const YEAR: &str = "2022";
+
+#[allow(dead_code)]
+const COOKIE_PATH: &str = "../session.cookie";
+
+#[allow(dead_code)]
+const INPUT_CACHE: &str = "input";
+
+fn main() {
+    println!("Hello, world!");
+}
